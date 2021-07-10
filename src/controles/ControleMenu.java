a2ca00cad0fe2ff13a910c3f7300a8fcbc568945
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2021-07-09 23:07:23 -0300

    Melhorias no codigo

--- src/controles/ControleMenu.java
+++ src/controles/ControleMenu.java
@@ -1,9 +1,11 @@
 package controles;
 
 import visoes.VisaoGasto;
+import visoes.VisaoLeitura;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ControleMenu {
     private int op;
@@ -15,25 +17,26 @@ public class ControleMenu {
     }
 
     public void trataOpcao (int op) throws IOException {
-        switch (op){
-            case 1:
-                //Inserir despesa
-                VisaoGasto visaoGasto = new VisaoGasto();
-                //File arquivo = new File("Teste.txt");
-                visaoGasto.preencheDados();
+            switch (op) {
+                case 1:
+                    //Inserir despesa
+                    VisaoGasto visaoGasto = new VisaoGasto();
+                    visaoGasto.preencheDados();
 
-                break;
-            case 2:
-                System.out.println("Entrei");
-                break;
-            case 3:
-                System.out.println("Entrei");
-                break;
-            default:
-                System.out.println("Ops... Opção invalida tente novamente");
+                    break;
+                case 2:
+                    //Leitura/Exibição de Gastos
+                    VisaoLeitura visaoLeitura = new VisaoLeitura();
+                    visaoLeitura.leituraDeArquivo();
+                    break;
+                case 3:
+                    System.out.println("Entrei");
+                    break;
+                default:
+                    System.out.println("Ops... Opção invalida tente novamente");
 
 
+            }//Fim switch
 
-        }//Fim switch
     }
 }
--- src/visoes/VisaoLeitura.java
+++ src/visoes/VisaoLeitura.java
@@ -1,4 +1,38 @@
 package visoes;
 
+import java.io.*;
+
 public class VisaoLeitura {
+    public VisaoLeitura (){
+
+    }
+
+    public void leituraDeArquivo() throws IOException {
+        File arq = new File("Teste.txt");
+        FileReader fr = new FileReader(arq);
+        BufferedReader br = new BufferedReader(fr);
+        double soma = 0;
+        while (br.ready()){
+            String linha = br.readLine();
+            String [] separador = linha.split(";");
+            //Convertendo String para Double
+            double valor = Double.parseDouble(separador[1]);
+            double parcelasPagas = Double.parseDouble(separador[3]);
+            double numeroDeParcelas = Double.parseDouble(separador[2]);
+            double  valorParcela=0;
+            System.out.println("Nome do gasto: "+separador[0]);
+            System.out.println("Valor do Gasto: "+ separador[1]);
+            System.out.printf("Parcelas: %.0f/%.0f\n", parcelasPagas, numeroDeParcelas);
+            if (parcelasPagas == numeroDeParcelas){
+                System.out.printf("Valor da %.0f parcela: %.2f\n", numeroDeParcelas, valorParcela = valor/numeroDeParcelas);
+            } else {
+                System.out.printf("Valor da %.0f parcela: %.2f\n", parcelasPagas+1, valorParcela = valor/numeroDeParcelas);
+            }
+            valor = valorParcela;
+            soma += valor;
+            System.out.println("-----------------------------");
+        }
+        System.out.printf("=== Valor Total: R$ %.2f ===\n\n",soma);
+        br.close();
+    }
 }
